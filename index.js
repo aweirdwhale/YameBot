61183c0fb3898764afd0187c07510b2f82f77c94
Reilov18 <76748500+Reilov18@users.noreply.github.com>
2021-06-19 15:30:27 +0200

    status

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client()
 
 const { token } = require('./config.json')
+let emojis = require('emojis');
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
@@ -9,6 +10,10 @@ client.events = new Discord.Collection();
 
 ['command_handler', 'event_handler'].forEach(handler => {
     require(`./handlers/${handler}`) (client, Discord)
+})
+
+client.once('ready', () => {
+    client.user.setActivity(emojis.unicode('des hentais :eyes:'), { type: 'WATCHING', url:  'https://twitch.tv/'})
 })
 
 //client.user.setActivity(emojis.unicode('des hentais :eyes:'), { type: 'WATCHING', url:  'https://twitch.tv/'})
