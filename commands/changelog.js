0077185a3f044019c442eb171d35aa05b2a7294d
Reilov18 <76748500+Reilov18@users.noreply.github.com>
2021-07-23 12:21:43 +0200

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -5,17 +5,16 @@ const fetch = require('node-fetch');
 
 module.exports = {
     name: 'changelog',
-    aliases: ['userversion', 'release'],
+    aliases: ['userversion'],
     description: "show iss position",
 
     async execute(client, message, args, cmd) {
 
         const embed = new Discord.MessageEmbed()
-            .setTitle("Version Release 1.1.3!", "https://github.com/Reilov18/YameBot/blob/release1.1.3/README.md")
+            .setTitle('Version Release 1.1.3!')
             .setImage("https://cdn.discordapp.com/attachments/657940718186266645/867785380350525460/releasetestimg2.png")
+            .setURL('https://github.com/Reilov18/YameBot/blob/release1.1.3/README.md')
             .setColor("RANDOM")
 
         message.channel.send(embed);
-
-        console.log('Version chargée')
 }}
